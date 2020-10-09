fd170ef5d209e3932fe6e04c93b49c8c70edec72
Roc <fangbaiyu@live.com>
2020-10-09 16:50:51 +0800

    chapter 5

--- src/Meowv.Blog.HttpApi/Controllers/BlogController.cs
+++ src/Meowv.Blog.HttpApi/Controllers/BlogController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.AspNetCore.Mvc;
@@ -29,5 +30,40 @@ namespace Meowv.Blog.Controllers
         {
             return await _blogService.InsertPostAsync(dto);
         }
+
+        /// <summary>
+        /// 删除博客
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public async Task<bool> DeletePostAsync([Required] int id)
+        {
+            return await _blogService.DeletePostAsync(id);
+        }
+
+        /// <summary>
+        /// 更新博客
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<bool> UpdatePostAsync([Required] int id, [FromBody] PostDto dto)
+        {
+            return await _blogService.UpdatePostAsync(id, dto);
+        }
+
+        /// <summary>
+        /// 查询博客
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<PostDto> GetPostAsync([Required] int id)
+        {
+            return await _blogService.GetPostAsync(id);
+        }
+
     }
 }
